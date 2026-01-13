fdfb0cb4c66d77c48447e14f4708946ccfdabfbf
Wael125 <wael.fraj.2023@ihec.ucar.tn>
2026-01-13 22:27:08 +0100

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('angular-test-deploy');
+  clicks = 0;
+
+  countClicks() {
+    this.clicks++;
+  }
 }
